5cc3aca89914ebcfb8cb8091fea3a3ac286be869
zhangbailong945 <1207549344@qq.com>
2017-10-26 23:12:36 +0800

    update

--- test.php
+++ test.php
@@ -1,23 +1,45 @@
 <?php
 
-$strArray=array('yytyys','abcabccaa','uuuxyy','mommentem');
-$countArray=array();
-for($i=0;$i<count($strArray);$i++)
+//载入一张图片,创建一张新图片
+$img=imagecreatefromjpeg('test.jpg');
+//获取图片的长和搞
+list($width,$height,$type)=getimagesize('test.jpg');
+
+$new_width=150;
+$new_height=50;
+//echo $new_width.'--'.$new_height.'<br/>';
+if($width<$height)
 {
-	//分割数组中的字符串
-    $arr=str_split($strArray[$i]);
-    //统计不重复字符的次数
-    $arr=array_count_values($arr);
-    $countArray[]=$arr;
+	$new_width=($new_height/$height)*$width;
 }
-
-function sortArray($array)
+else
 {
-	for($j=0;$j<count($countArray);$j++)
-	{
-		
-	}	
+	$new_height=($new_width/$width)*$height;
 }
+//echo $new_width.'--'.$new_height.'<br/>';
+
+/*按百分比
+$per=0.3;
+$newWidth=$width*$per;
+$newHeight=$height*$per;
+*/
+//创建新图像，用上缩放后的大小
+$new=imagecreatetruecolor($new_width,$new_height);
+imagecopyresampled($new,$img,0,0,0,0,$new_width,$new_height,$width,$height);
+
+$color=imagecolorallocate($new,0,0,0);
+//文字水印
+imagettftext($new,10,0,10,10,$color,'includes/Elephant.ttf','zhangbailong.com');
+//图片水印
+$mark=imagecreatefrompng('shuiyin.png');
+imagecopy($new,$mark,10,10,0,0,140,20);
 
 
+//定义编码格式
+header('Content-Type:image/jpeg');
+//输出图片
+imagejpeg($new);
+
+//销毁资源
+imagedestroy($img);
 ?>
